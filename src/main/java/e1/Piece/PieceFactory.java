4d1790e094bea1142988c583768951442e00f668
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-03-10 16:54:17 +0100

    Complete es 1 step 2

--- src/main/java/e1/Piece/PieceFactory.java
+++ src/main/java/e1/Piece/PieceFactory.java
@@ -4,12 +4,12 @@ import e1.Pair;
 
 public class PieceFactory {
 
-	public static Knight createKnight(Pair<Integer, Integer> position) {
-        return new Knight(position);
-	}
+        public static Knight createKnight(Pair<Integer, Integer> position) {
+                return new Knight(position);
+        }
 
-    public static Pawn createPawn(Pair<Integer, Integer> position) {
-        return new Pawn(position);
-	}
+        public static Pawn createPawn(Pair<Integer, Integer> position) {
+                return new Pawn(position);
+        }
 
 }
--- src/test/java/e1/Piece/PieceTest.java
+++ src/test/java/e1/Piece/PieceTest.java
@@ -1,15 +1,48 @@
 package e1.Piece;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public abstract class PieceTest {
+import e1.Pair;
 
-    protected Piece piece;
+public class PieceTest {
+
+    private Knight knight;
+    private Pawn pawn;
+
+    @BeforeEach
+    public void beforeEach(){
+        this.knight = PieceFactory.createKnight(new Pair<Integer, Integer>(0,0));
+        this.pawn = PieceFactory.createPawn(new Pair<Integer, Integer>(1,1));
+    }
 
     @Test
-    public void testCreatePiece() {
-        assertNotNull(this.piece.getPosition());
+    public void testKnightMove(){
+        Pair<Integer, Integer> newPosition = new Pair<Integer, Integer>(1,2);
+        this.knight.move(newPosition);
+        assertEquals(newPosition, this.knight.getPosition());
+    }
+
+    @Test
+    public void testIllegalKnightMove(){
+        Pair<Integer, Integer> oldPosition = this.knight.getPosition();
+        Pair<Integer, Integer> wrongPosition = new Pair<Integer, Integer>(2,2);
+        ((Knight) this.knight).move(wrongPosition);
+        assertEquals(oldPosition, this.knight.getPosition());
+    }
+
+
+    @Test
+    public void testCreateKnight() {
+        assertNotNull(this.knight.getPosition());
+    }
+
+    @Test
+    public void testCreatePawn() {
+        assertNotNull(this.pawn.getPosition());
     }
 
 }
